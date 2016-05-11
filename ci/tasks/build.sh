e3ede53271fc16aed23041a9767b7f64c69b07d2
Patrick Crocker <patrick.crocker@gmail.com>
2016-05-11 12:37:02 -0500

    updated artifactId

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,4 +6,4 @@ pushd source-code
   ./mvnw clean package
 popd
 
-cp source-code/target/concourse-example-maven-simple-0.0.1-SNAPSHOT.jar  build-output/.
+cp source-code/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar  build-output/.
